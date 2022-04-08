30e0cd7ad589fb89ab848607ebac1727f9af8d6d
FandyyyDev <nazril.afandi1603@gmail.com>
2022-04-08 15:01:25 +0700

    y

---
+++ ryudata/promosi.js
@@ -0,0 +1,39 @@
+exports.promosi1 = () => {
+	return`Jasarun Script?
+	
+Minat Chat?
+wa.me/6285609233482
+`
+}
+
+exports.promosi2 = () => {
+	return`Jasarun Script?
+	
+Minat Chat?
+wa.me/6285609233482
+`
+}
+
+exports.promosi3 = () => {
+	return`Jasarun Script?
+	
+Minat Chat?
+wa.me/6285609233482
+`
+}
+
+exports.promosi4 = () => {
+	return`Jasarun Script?
+	
+Minat Chat?
+wa.me/6285609233482
+`
+}
+
+exports.promosi5 = () => {
+	return`Jasarun Script?
+	
+Minat Chat?
+wa.me/6285609233482
+`
+}
